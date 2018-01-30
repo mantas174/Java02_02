6aef05259191c1b586594c788c018019ee8f8ceb
Mantas <mantas.muzzz@gmail.com>
2018-01-30 15:00:49 +0200

    Padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner in = new Scanner(System.in);
+	    String zodis;
+
+	    System.out.println("Iveskite zodi: ");
+	    zodis = in.nextLine();
+
+	    System.out.println("Zodzio ilgis: " + zodis.length());
     }
 }
